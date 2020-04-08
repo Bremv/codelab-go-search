5ebc29abd46a56f7dfdb3ea15887ad69d396f56e
Богдан Куликов <realggaster@gmail.com>
2020-04-08 18:45:58 +0300

    Solved #1, #2, #3 from README

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
+	"sync"
 )
 
 var (
 	recursiveFlag = flag.Bool("r", false, "recursive search: for directories")
+	numberingFlag = flag.Bool("n", false, "show line numbers")
+	wg            sync.WaitGroup
 )
 
 type ScanResult struct {
@@ -18,25 +22,32 @@ type ScanResult struct {
 	line       string
 }
 
-func scanFile(fpath, pattern string) ([]string, error) {
+// []string
+func scanFile(fpath, pattern string) ([]ScanResult, []int, error) {
 	f, err := os.Open(fpath)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 	result := make([]string, 0)
+	res := make([]ScanResult, 0)
+	lines := make([]int, 0)
+	lineNum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
+		lineNum++
 		if strings.Contains(line, pattern) {
+			res = append(res, ScanResult{file: fpath, lineNumber: lineNum, line: line})
 			result = append(result, line)
+			lines = append(lines, lineNum)
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return result, nil
+	return res, lines, nil
 }
 
 func exit(format string, val ...interface{}) {
@@ -49,18 +60,38 @@ func exit(format string, val ...interface{}) {
 	os.Exit(1)
 }
 
-func processFile(fpath string, pattern string) {
-	res, err := scanFile(fpath, pattern)
+func processFile(fpath string, pattern string, numbering bool) {
+	result, _, err := scanFile(fpath, pattern)
 	if err != nil {
 		exit("Error scanning %s: %s", fpath, err.Error())
 	}
-	for _, line := range res {
-		fmt.Println(line)
+	if numbering {
+		for _, res := range result {
+			fmt.Printf("%s:%d : %s\n", res.file, res.lineNumber, res.line)
+			//fmt.Printf("%s:%d : %s\n", fpath, lines[i], line)
+		}
+	} else {
+		for _, res := range result {
+			fmt.Printf("%s: %s\n", res.file, res.line)
+		}
 	}
+	wg.Done()
 }
 
-func processDirectory(dir string, pattern string) {
+func processDirectory(dir string, pattern string, numbering bool) {
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if !info.IsDir() {
+			wg.Add(1)
+			go processFile(dir+info.Name(), pattern, numbering)
+		}
 
+		wg.Wait()
+		return nil
+	})
 }
 
 func main() {
@@ -84,8 +115,8 @@ func main() {
 	}
 
 	if info.IsDir() && recursive {
-		processDirectory(path, pattern)
+		processDirectory(path, pattern, *numberingFlag)
 	} else {
-		processFile(path, pattern)
+		processFile(path, pattern, *numberingFlag)
 	}
 }
